09428e467db6c4b90f5f488d4903903db654acc0
amuchin32 <feolytogba20@gmail.com>
2026-05-24 18:54:02 -0500

    Add afrilens.vercel.app to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://afrilens-pi.vercel.app', process.env.CLIENT_URL].filter(Boolean),
+  origin: ['http://localhost:5173', 'https://afrilens-pi.vercel.app', 'https://afrilens.vercel.app', process.env.CLIENT_URL].filter(Boolean),
   credentials: true,
 }));
 
@@ -52,6 +52,7 @@ app.use((req, res) => {
 app.use(require('./middleware/errorHandler'));
 
 module.exports = app;
+
 
 
 
